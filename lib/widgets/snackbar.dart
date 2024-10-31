66fb0c08f5972c2a9a7502345e6e8bda3a2dcf73
davidlealo <davidlealo@gmail.com>
2024-10-30 21:33:24 -0300

    agregar el modificador required para snackBarAction

--- lib/widgets/snackbar.dart
+++ lib/widgets/snackbar.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
-class CustomSnackBar {
-  CustomSnackBar(BuildContext context, Widget content,
-      {SnackBarAction snackBarAction, Color backgroundColor = Colors.green}) {
-    final SnackBar snackBar = SnackBar(
-        action: snackBarAction,
-        backgroundColor: backgroundColor,
-        content: content,
-        behavior: SnackBarBehavior.floating);
+class CustomSnackBar extends StatelessWidget {
+  const CustomSnackBar({
+    required this.snackBarAction,
+    this.backgroundColor = Colors.green,
+    Key? key,
+  }) : super(key: key);
 
-    ScaffoldMessenger.of(context).hideCurrentSnackBar();
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  final SnackBarAction snackBarAction;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return SnackBar(
+      action: snackBarAction,
+      backgroundColor: backgroundColor,
+      content: const Text('This is a custom snackbar'),
+    );
   }
 }
